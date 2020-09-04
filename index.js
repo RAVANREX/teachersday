0f38deb374d8e5f8768ae767a2fe4b21fd3c92fc
RAVANREX <myslfsumansaha@gmail.com>
2020-09-05 02:06:56 +0530

    commited

---
+++ index.js
@@ -0,0 +1,51 @@
+ document.onreadystatechange = function() {
+    var state = document.readyState
+    if (state == 'complete') {
+      setTimeout(function() {
+        document.getElementById('wrap-loader').remove();
+        document.getElementById('container-fluid').style.visibility = "visible";
+      },3000);
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    var div = $('.autoscroller');
+
+    $('.autoscroller').bind('scroll mousedown wheel DOMMouseScroll mousewheel keyup', function(evt) {
+        if (evt.type === 'DOMMouseScroll' || evt.type === 'keyup' || evt.type === 'mousewheel') {
+    
+        }
+        if (evt.originalEvent.detail < 0 || (evt.originalEvent.wheelDelta && evt.originalEvent.wheelDelta > 0)) { 
+            clearInterval(autoscroller);
+        }
+        if (evt.originalEvent.detail > 0 || (evt.originalEvent.wheelDelta && evt.originalEvent.wheelDelta < 0)) { 
+            clearInterval(autoscroller);
+        }
+    });
+    
+    var autoscroller = setInterval(function(){
+        var pos = div.scrollTop();
+        if ((div.scrollTop() + div.innerHeight()) >= div[0].scrollHeight) {
+            clearInterval(autoscroller);
+        }
+        div.scrollTop(pos + 1);
+    }, 50);
+
+    
+  }
+
+
+
+ 
+    
+   
